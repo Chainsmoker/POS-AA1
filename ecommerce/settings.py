332e09611509b6fa041e5e9a55561d889c18a2a1
Alejandro <75154124+Chainsmoker@users.noreply.github.com>
2024-05-12 22:57:41 -0500

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -131,6 +131,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "static/",
+]
+STATIC_ROOT = 'staticfiles/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
 
